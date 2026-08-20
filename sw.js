d8a6a505a2ab9d18e8304d3b04655ae2f88aa970
Sound News <victorchitive1@gmail.com>
2026-08-20 20:15:33 +0200

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sound-news-v2'; // Altera para v3, v4... sempre que fizeres atualizações!
+const CACHE_NAME = 'sound-news-v3'; // Altera para v3, v4... sempre que fizeres atualizações!
 
 const urlsToCache = [
   './',
